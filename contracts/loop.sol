e335576df4d37803a98a89b746315b5306accdda
feranmiba <amiolademilade@gmail.com>
2023-11-29 13:54:07 +0100

    loop-sol

---
+++ contracts/loop.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0
+
+
+pragma solidity >=0.8.9;
+
+
+
+
+contract LoopExample {
+
+    //A simple function that loops 1000 times 
+    function complexLoop(uint _score) external pure returns(uint) {
+         _score;
+
+          for (uint i = 0; i < 1000; i++) {
+            _score += i + 2; 
+    }
+    return _score;
+    }
+
+    // A complex function that loops 100 times
+
+    function loop(uint _car) external pure returns(uint) {
+        _car;
+
+        for (uint i = 0; i < 100; i++) {
+            if(_car == 50) {
+                _car *= i;
+            }
+        }
+        return _car;
+    } 
+  
+}
